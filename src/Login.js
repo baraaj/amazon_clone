5b7f07e5137e26b1098e02320936e63825801c9d
baraaj <jeridibaraa@gmail.com>
2022-10-11 21:20:05 +0200

    login commit

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Login.css';
 
 const Login = () => {
     return (
@@ -12,6 +13,19 @@ const Login = () => {
             </Link>
 
             <div className='login__container'>
+            <h1>Sign-in</h1>
+                <form>
+                    <label>Username</label>
+                    <input type="text"></input>
+                    <label>Password</label>
+                    <input type="password"></input>
+                    <button type='submit' className='login__signInButton'>Sign In</button>
+                </form>
+                <p>
+                    By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
+                    see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                </p>
+                <button className='login__registerButton'>Create your Amazon Account</button>
 
             </div>
             
