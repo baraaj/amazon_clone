8925157ede307bdb04d69daf986ef04474498eb9
baraaj <jeridibaraa@gmail.com>
2022-10-25 16:48:57 +0200

    bd commit

--- functions/index.js
+++ functions/index.js
@@ -2,4 +2,14 @@ const functions = require("firebase-functions");
 const express =require("express");
 const cors=require("cors");
 const stripe=require('stripe')
-('pk_test_51LsBjKGy7cHbJhlZA49fmnQZDvvMGPbNXyNxs4WLyv8LJ2lCHtxCG8uBpgmtXXjek7RCj4RoYWGAjqa2MOIWCwhP00nbBIXJxf')
+('pk_test_51LsBjKGy7cHbJhlZA49fmnQZDvvMGPbNXyNxs4WLyv8LJ2lCHtxCG8uBpgmtXXjek7RCj4RoYWGAjqa2MOIWCwhP00nbBIXJxf');
+
+// API
+
+// - App config
+const app = express();
+
+// - Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+// - API routes
