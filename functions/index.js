16151b30123fd7aea4870be2f35b65eacebded92
baraaj <jeridibaraa@gmail.com>
2022-10-25 16:22:52 +0200

    bd commit

--- functions/index.js
+++ functions/index.js
@@ -1,2 +1,5 @@
 const functions = require("firebase-functions");
-const express =require("express")
+const express =require("express");
+const cors=require("cors");
+const stripe=require('stripe')
+('pk_test_51LsBjKGy7cHbJhlZA49fmnQZDvvMGPbNXyNxs4WLyv8LJ2lCHtxCG8uBpgmtXXjek7RCj4RoYWGAjqa2MOIWCwhP00nbBIXJxf')
